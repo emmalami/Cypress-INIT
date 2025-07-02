ba7ef748b2e8f6d868e9fba410e2b3911602713c
Olamide John <Olamide.John@Interswitchng.com>
2025-07-02 14:47:14 +0100

    Login and dashboard

--- cypress.config.js
+++ cypress.config.js
@@ -1,5 +1,11 @@
 module.exports = {
+  defaultCommandTimeout: 10000,
+  pageLoadTimeout: 60000,
+  requestTimeout: 15000,
+  responseTimeout: 30000,
   e2e: {
+    experimentalStudio: true,
+    baseUrl: 'https://dps-admin-ui.qa.interswitchng.com',
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
--- cypress/e2e/Login and Dashoard Page.cy.js
+++ cypress/e2e/Login and Dashoard Page.cy.js
@@ -1,8 +1,43 @@
+
+
+
 describe('Admin Login page', () => {
-  it('login', () => {
-    cy.visit('https://dps-admin-ui.qa.interswitchng.com/')
+
+  beforeEach(() => {
+    // Visit and log in before each test
+    cy.visit('https://dps-admin-ui.qa.interswitchng.com/');
+
     cy.get('input[type="email"]').type('olamide.john@interswitchng.com');
     cy.get('.MuiInputBase-root.css-1mbvqtf').find('input[type="password"]').type('Testqa1101!@#$%^');
     cy.get('button[type="submit"]').click();
-  })
-})
+  });
+
+  it('Dashboard interactions', () => {
+    cy.get('[data-testid="GridViewIcon"]').should('be.visible').click();
+    
+    cy.get('[data-testid="AddIcon"]')
+      .parents('button')
+      .should('contain.text', 'Add new company')
+      .click({ force: true });
+
+    cy.get('[data-testid="KeyboardBackspaceIcon"]')
+      .should('exist')
+      .parents('span[title="Back"]')
+      .click();
+
+    cy.get(':nth-child(27) > .MuiPaper-root > .MuiCardContent-root').click();
+    cy.get('[data-testid="GridViewIcon"]').click();
+    cy.get('[data-testid="AccountCircleIcon"]').click();
+    cy.get('.MuiBackdrop-root').click();
+
+    cy.get('[data-testid="NotificationsIcon"] > path').click();
+    cy.get('.MuiInputBase-input').clear().type('quick');
+    cy.get('.MuiCardContent-root').click();
+
+    cy.get('[data-testid="GridViewIcon"] > path').click({ force: true });
+    cy.get('.top__title__has__button > .MuiButtonBase-root').click();
+    cy.get('.MuiButton-contained').click();
+
+    cy.get('[data-testid="GridViewIcon"]').should('be.visible').click();
+  });
+});
