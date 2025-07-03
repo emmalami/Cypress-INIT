195fac1c42a5aa512d710ccab464170ab46c3547
Olamide John <Olamide.John@Interswitchng.com>
2025-07-03 14:35:24 +0100

    user management

--- cypress/e2e/Login and Dashoard Page.cy.js
+++ cypress/e2e/Login and Dashoard Page.cy.js
@@ -40,4 +40,43 @@ describe('Admin Login page', () => {
 
     cy.get('[data-testid="GridViewIcon"]').should('be.visible').click();
   });
-});
+  it.only('Add Account flow', () => {
+    cy.get('[data-testid="GridViewIcon"]').click();
+    cy.get('.top__title__has__button > .MuiButtonBase-root').click();
+    cy.get('.formarea__main > :nth-child(1) > .MuiInputBase-root > .MuiInputBase-input').clear('Al halil');
+    cy.get('.formarea__main > :nth-child(1) > .MuiInputBase-root > .MuiInputBase-input').type('Al halil1');
+    cy.get(':nth-child(3) > .MuiInputBase-root > .MuiInputBase-input').clear('R');
+    cy.get(':nth-child(3) > .MuiInputBase-root > .MuiInputBase-input').type('Ronaldo');
+    cy.get(':nth-child(4) > .MuiInputBase-root > .MuiInputBase-input').clear('g');
+    cy.get(':nth-child(4) > .MuiInputBase-root > .MuiInputBase-input').type('Goated');
+    cy.get(':nth-child(5) > .MuiInputBase-root > .MuiInputBase-input').click();
+    cy.get(':nth-child(5) > .MuiInputBase-root > .MuiInputBase-input').clear('l');
+    cy.get(':nth-child(5) > .MuiInputBase-root > .MuiInputBase-input').type('olamide.john@interswitchng.com');
+    cy.get('#corporate_country').clear('n');
+    cy.get('#corporate_country').type('nig');
+    cy.get('#corporate_country-option-1').click();
+    cy.get('body').click();
+    cy.get('#mui-component-select-corporate_currency').click(); // Open dropdown
+    cy.get('li[data-value="175"]')
+     .should('be.visible').and('contain.text', 'Nigerian naira (₦)').click();
+    cy.get('body').click();
+    cy.get('#mui-component-select-corporate_language').click()
+    cy.get('li[data-value="280"]').should('contain.text', 'English (eng)').click()
+    cy.get('.css-h9xt8w > .MuiButton-contained').click();
+    /*
+    cy.get(':nth-child(1) > .permission__mgmt__r > .css-vb6e92 > .MuiSwitch-root > .MuiButtonBase-root > .PrivateSwitchBase-input').check();
+    cy.get(':nth-child(1) > .permission__mgmt__r > .css-vb6e92 > .MuiSwitch-root > .MuiButtonBase-root > .PrivateSwitchBase-input').uncheck();
+    cy.get('.MuiFormControlLabel-root > .MuiButtonBase-root > .PrivateSwitchBase-input').check();
+    cy.get('.MuiFormControlLabel-root > .MuiButtonBase-root > .PrivateSwitchBase-input').uncheck();
+    cy.get('.MuiFormControlLabel-root > .MuiButtonBase-root > .PrivateSwitchBase-input').check();
+    cy.get('.MuiButton-contained').click();
+    cy.get(':nth-child(1) > .MuiInputBase-root > .MuiInputBase-input').clear('O');
+    cy.get(':nth-child(1) > .MuiInputBase-root > .MuiInputBase-input').type('InterswitchNg');
+    cy.get(':nth-child(2) > .MuiInputBase-root > .MuiInputBase-input').clear('0');
+    cy.get(':nth-child(2) > .MuiInputBase-root > .MuiInputBase-input').type('012345678');
+    cy.get('body').click();
+    cy.get('[data-value="uat"]').click();
+    cy.get('[style="position: absolute; right: 50%;"]').click();
+    cy.get('.MuiButton-contained').click();  */
+  });
+})
---
+++ cypress/e2e/userManagement.cy.js
@@ -0,0 +1,33 @@
+describe('Corporate Regression', () => {
+
+  beforeEach(() => {
+    // Visit and log in before each test
+    cy.visit('https://dps-admin-ui.qa.interswitchng.com/');
+
+    cy.get('input[type="email"]').type('olamide.john@interswitchng.com');
+    cy.get('.MuiInputBase-root.css-1mbvqtf').find('input[type="password"]').type('Testqa1101!@#$%^');
+    cy.get('button[type="submit"]').click();
+  });
+
+  it('User Management', () => {
+    cy.get('[data-testid="GridViewIcon"]').click();
+    cy.get('.MuiInputBase-input').clear('re');
+    cy.get('.MuiInputBase-input').type('regression');
+    cy.get('.MuiCardContent-root').click();
+    cy.get('.fix__menu__list > :nth-child(1) > .MuiButtonBase-root > .MuiListItemText-root > .MuiTypography-root').click();
+    cy.get('.search__grid > .MuiButtonBase-root').click();
+    cy.get(':nth-child(2) > .MuiFormGroup-root > .MuiInputBase-root > .MuiInputBase-input').clear('O');
+    cy.get(':nth-child(2) > .MuiFormGroup-root > .MuiInputBase-root > .MuiInputBase-input').type('Olamide');
+    cy.get(':nth-child(3) > .MuiFormGroup-root > .MuiInputBase-root > .MuiInputBase-input').clear('j');
+    cy.get(':nth-child(3) > .MuiFormGroup-root > .MuiInputBase-root > .MuiInputBase-input').type('john');
+    cy.get(':nth-child(4) > .MuiFormGroup-root > .MuiInputBase-root > .MuiInputBase-input').clear('o');
+    cy.get(':nth-child(4) > .MuiFormGroup-root > .MuiInputBase-root > .MuiInputBase-input').type('test695@interswitchng.com');
+    cy.get('.css-8mmkcg').click();
+    cy.get('#react-select-2-option-0').click();
+    cy.get('.MuiButton-contained').click();
+    cy.contains('button', 'Submit',{timeout:30000}).click({ force: true })
+    cy.get('[data-testid="PlayCircleFilledWhiteIcon"]',{timeout:30000}).eq(1).click({ force: true })
+    cy.get('.MuiSwitch-root > .MuiButtonBase-root > .PrivateSwitchBase-input').check();
+    cy.get('.MuiSwitch-root > .MuiButtonBase-root > .PrivateSwitchBase-input').check();
+  });
+})
