7f714f3841a6248e3450f62d5b008596b2c91057
Olamide John <olamide.john@outlook.com>
2025-07-16 14:19:13 +0100

    config update

--- cypress.config.js
+++ cypress.config.js
@@ -1,49 +1,34 @@
-module.exports = {
+const { defineConfig } = require('cypress');
+
+module.exports = defineConfig({
+  projectId: '3b8kd4', // Cypress Dashboard project ID
+
   defaultCommandTimeout: 10000,
   pageLoadTimeout: 60000,
   requestTimeout: 15000,
   responseTimeout: 30000,
-  e2e: {
-    experimentalStudio: true,
-    baseUrl: 'https://dps-admin-ui.qa.interswitchng.com',
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-    },
-  },
-};
-const { defineConfig } = require("cypress");
 
-module.exports = defineConfig({
-  projectId: '3b8kd4', // Your Cypress Dashboard project ID
-  "reporter": "cypress-multi-reporters",
-  "reporterOptions":{
-    "reporterEnabled": "mochawesome",
-    "mochawesomeReporterOptions":{
-      "reportDir":"cypress/reports/mocha",
-      "quiet": true,
-      "overwrite": false,
-      "html":false,
-      "json": true
-
+  reporter: 'cypress-multi-reporters',
+  reporterOptions: {
+    reporterEnabled: 'mochawesome',
+    mochawesomeReporterOptions: {
+      reportDir: 'cypress/reports/mocha',
+      quiet: true,
+      overwrite: false,
+      html: false,
+      json: true,
     }
   },
 
   e2e: {
-    // Setup Node events (plugins)
+    experimentalStudio: true,
+    baseUrl: 'https://dps-admin-ui.k8.isw.la',
+
+    supportFile: 'cypress/support/e2e.js',
+
     setupNodeEvents(on, config) {
+      // Load plugins
       return require('./cypress/plugins/index.js')(on, config);
-    },
-
-    // Path to your custom support file
-    supportFile: "cypress/support/e2e.js",
-  },
+    }
+  }
 });
-
-"cypress": "^14.2.0",
-        "cypress-multi-reporters": "^2.0.5",
-        "mocha": "^11.4.0",
-        "mochawesome": "^7.1.3",
-        "mochawesome-merge": "^5.0.0",
-        "mochawesome-report-generator": "^6.2.0",
-        "xlsx": "^0.18.5",
-        "xpath": "^0.0.34"
